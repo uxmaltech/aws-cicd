21497b88d45e1c02e4f33cc5f15aa10206d0c2d2
Enrique Martinez <enmaca@hotmail.com>
2024-01-31 17:35:29 -0600

    WIP 31-ENERO-2024 1
    MVP - Refactoring build base images.
    
         apache-php
         nginx
         php-fpm
         php-octane
         php-cli

--- src/Command/Docker/BuildBaseImagesCommand.php
+++ src/Command/Docker/BuildBaseImagesCommand.php
@@ -5,11 +5,13 @@ namespace Uxmal\Devtools\Command\Docker;
 use Illuminate\Console\Command;
 use JetBrains\PhpStorm\NoReturn;
 use Random\RandomException;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Uxmal\Devtools\Traits\DockerUtils;
 use Uxmal\Devtools\Traits\GeneralUtils;
 
 class BuildBaseImagesCommand extends Command
 {
-    use GeneralUtils;
+    use GeneralUtils, DockerUtils;
 
     /**
      * The console command name.
@@ -49,100 +51,163 @@ class BuildBaseImagesCommand extends Command
     #[NoReturn]
     protected function buildDockerImage(): void
     {
-        $this->line('Building the base docker image for '.config('uxmaltech.name').' ...');
+        $this->line('Building the base docker image for ' . config('uxmaltech.name') . ' ...');
         $this->newLine();
 
         $headers = ['Variable', 'Contenido'];
 
-        $ecr_proxy = config('aws-ecr.proxy');
-        // Mostrar la tabla
-        $envTable = [
-            ['Build Tag', $this->ecrImageTag],
-            ['Ecr Proxy', $ecr_proxy],
-
-        ];
-
         $prefix = config('uxmaltech.prefix', 'uxtch');
 
-        $ecr_php_fpm = config('aws-ecr.php-fpm');
-        if ($ecr_php_fpm) {
-            $ecr_php_fpm_versioned_tag = $prefix.'-php-fpm'.':'.$this->ecrImageTag;
-            $ecr_php_fpm_repository = $ecr_proxy.'/'.$prefix.'-php-fpm';
-            $ecr_php_fpm_base_image = $ecr_php_fpm['base-image'];
-            $ecr_php_fpm_exposed_port = $ecr_php_fpm['exposed-port'];
-
-            $envTable = array_merge($envTable, [
-                ['Ecr PHP FPM base image', $ecr_php_fpm_base_image],
-                ['Ecr PHP FPM repository', $ecr_php_fpm_repository],
-                ['Ecr PHP FPM versioned tag', $ecr_php_fpm_versioned_tag],
-                ['Ecr PHP FPM exposed ports', $ecr_php_fpm_exposed_port],
-            ]);
-        }
-
-        $ecr_nginx = config('aws-ecr.nginx');
-        if ($ecr_nginx) {
-            $ecr_nginx_versioned_tag = 'uxtch-nginx'.':'.$this->ecrImageTag;
-            $ecr_nginx_repository = $ecr_proxy.'/'.$prefix.'-nginx';
-            $ecr_nginx_base_image = $ecr_nginx['base-image'];
-            $ecr_nginx_exposed_port = $ecr_nginx['exposed-port'];
-
-            $envTable = array_merge($envTable, [
-                ['Ecr Nginx base image', $ecr_nginx_base_image],
-                ['Ecr Nginx repository', $ecr_nginx_repository],
-                ['Ecr Nginx versioned tag', $ecr_nginx_versioned_tag],
-                ['Ecr Nginx exposed port', $ecr_nginx_exposed_port],
-            ]);
-        }
+        $currentDate = date('dmY');
+        $hours = date('G');   // Hours since midnight (0-23)
+        $minutes = date('i'); // Minutes (00-59)
+        $seconds = date('s'); // Seconds (00-59)
+        $secondsOfDay = ((int)$hours * 3600) + ((int)$minutes * 60) . (int)$seconds;
+        $versionString = $currentDate . $hours . $minutes . $seconds;
 
         /**
-         * ,
-         * ['PHP_FPM_BASE_IMAGE', $this->clusterContainerPhpFpmBaseImage],
-         * ['PHP_FPM_BASE_TAG', $this->clusterContainerPhpFpmTag],
-         * ['NGINX_BASE_REPOSITORY', $this->clusterContainerNginxRepository],
-         * ['NGINX_BASE_IMAGE', $this->clusterContainerNginxBaseImage],
-         * ['NGINX_BASE_TAG', $this->clusterContainerNginxTag],
+         * $alpineVersion = '3.19';
+         * $phpVersion = '8.2.15';
+         * $nginxVersion = '1.24';
+         * $apacheVersion = '2.4.52';
          */
+
+        $envTable = [];
+        $apache_php_base_image = $prefix . '-apache' . $this->apacheVersion . '-php' . $this->phpVersion . '-alpine' . $this->alpineVersion;
+        $nginx_base_image = $prefix . '-nginx' . $this->nginxVersion . '-alpine' . $this->alpineVersion;
+        $php_fpm_image = $prefix . '-php-fpm' . $this->phpVersion . '-alpine' . $this->alpineVersion;
+        $php_cli_octane_image = $prefix . '-php-' . $this->phpVersion . '-octane-alpine' . $this->alpineVersion;
+        $php_cli_image = $prefix . '-php-cli-' . $this->phpVersion . '-alpine' . $this->alpineVersion;
+        $dockerizedImages = config('dockerized.images');
+
+        $envTable = array_merge($envTable, [
+            ['Build Version', $versionString],
+            ['Apache PHP Image', $apache_php_base_image],
+            ['Nginx Image', $nginx_base_image],
+            ['PHP-FPM Image', $php_fpm_image],
+            ['Octane PHP-Cli Swoole', $php_cli_octane_image],
+            ['PHP-Cli Image', $php_cli_image],
+            new TableSeparator(),
+            ['Images To Build', implode(',', array_keys($dockerizedImages))],
+        ]);
+
         $this->table($headers, $envTable);
 
-        $this->newLine();
-        if (! $this->confirm('Do you wish to continue?', false)) {
-            $this->info('Bye!');
-            exit(0);
+        if (!$this->confirm('¿Es correcto, proceder a la creación de la VPC?')) {
+            $this->error('Abortando...');
+            exit(1);
         }
 
-        /**
-         * php-fpm
-         */
-        if ($ecr_php_fpm) {
-            $workDir = $this->laravel->basePath('docker-images/base-images/uxtch-php-fpm/');
-            if (! is_dir($workDir)) {
-                $this->error('The directory docker-images/base-images/uxtch-php-fpm does not exists.');
-                exit(1);
-            }
-            $ecr_proxy_php_fpm_tag = $ecr_php_fpm_repository.':'.$this->ecrImageTag;
-            $latest_php_fpm_tag = $ecr_php_fpm_repository.':latest';
-            $this->runDockerCmd(['build', '.', '-t', $ecr_php_fpm_versioned_tag], $workDir);
-            $this->runDockerCmd(['tag', $ecr_php_fpm_versioned_tag, $ecr_proxy_php_fpm_tag], $workDir);
-            $this->runDockerCmd(['tag', $ecr_php_fpm_versioned_tag, $latest_php_fpm_tag], $workDir);
-            $this->info("Docker image built $ecr_php_fpm_versioned_tag successfully.");
-            $this->replaceConfigKey('php-fpm.latest-base-image', $ecr_php_fpm_versioned_tag, false, config_path('aws-ecr.php'));
+        foreach (array_keys($dockerizedImages) as $imageToBuild) {
+            switch ($imageToBuild) {
+                case 'apache-php':
+                    $this->buildApachePhpImage($apache_php_base_image, $versionString);
+                    break;
+                case 'nginx':
+                    $this->buildNginxImage($nginx_base_image, $versionString);
+                    break;
+                case 'php-fpm':
+                    $this->buildPhpFpmImage($php_fpm_image, $versionString);
+                    break;
+                case 'php-cli-octane':
+                    $this->buildPhpCliOctaneImage($php_cli_octane_image, $versionString);
+                    break;
+                case 'php-cli':
+                    $this->buildPhpCliImage($php_cli_image, $versionString);
+                    break;
 
-        }
-
-        if ($ecr_nginx) {
-            $workDir = $this->laravel->basePath('docker-images/base-images/uxtch-nginx/');
-            if (! is_dir($workDir)) {
-                $this->error('The directory docker-images/base-images/uxtch-nginx does not exists.');
-                exit(1);
             }
-            $ecr_proxy_nginx_tag = $ecr_nginx_repository.':'.$this->ecrImageTag;
-            $latest_nginx_tag = $ecr_nginx_repository.':latest';
-            $this->runDockerCmd(['build', '.', '-t', $ecr_nginx_versioned_tag], $workDir);
-            $this->runDockerCmd(['tag', $ecr_nginx_versioned_tag, $ecr_proxy_nginx_tag], $workDir);
-            $this->runDockerCmd(['tag', $ecr_nginx_versioned_tag, $latest_nginx_tag], $workDir);
-            $this->info("Docker image built $ecr_nginx_versioned_tag successfully.");
-            $this->replaceConfigKey('nginx.latest-base-image', $ecr_nginx_versioned_tag, false, config_path('aws-ecr.php'));
         }
         exit(0);
+    }
+
+    private function buildApachePhpImage($image, $versionString): void
+    {
+        $workDir = __DIR__ . '/base-images/uxtch-apache-php/';
+        if (!is_dir($workDir)) {
+            $this->error('The directory docker-images/base-images/uxtch-apache-php does not exists.');
+            exit(1);
+        }
+        $this->info("Build Image: $image Tag: $versionString");
+
+        $this->runDockerCmd(['build', '.', '-t', $image . ':' . $versionString], $workDir);
+
+        $this->runDockerCmd(['tag', $image . ':' . $versionString, $image . ':latest'], $workDir);
+
+        $this->info("Docker image built $image:$versionString successfully.");
+
+        $this->replaceConfigKey('images.apache-php.base-image', $image . ':' . $versionString, true, config_path('dockerized.php') );
+    }
+
+    private function buildNginxImage($image, $versionString): void
+    {
+        $workDir = __DIR__ . '/base-images/uxtch-nginx/';
+        if (!is_dir($workDir)) {
+            $this->error('The directory docker-images/base-images/uxtch-nginx does not exists.');
+            exit(1);
+        }
+        $this->info("Build Image: $image Tag: $versionString");
+
+        $this->runDockerCmd(['build', '.', '-t', $image . ':' . $versionString], $workDir);
+
+        $this->runDockerCmd(['tag', $image . ':' . $versionString, $image . ':latest'], $workDir);
+
+        $this->info("Docker image built $image:$versionString successfully.");
+
+        $this->replaceConfigKey('images.nginx.base-image', $image . ':' . $versionString, true, config_path('dockerized.php') );
+    }
+
+    private function buildPhpFpmImage($image, $versionString): void
+    {
+        $workDir = __DIR__ . '/base-images/uxtch-php-fpm/';
+        if (!is_dir($workDir)) {
+            $this->error('The directory docker-images/base-images/uxtch-php-fpm does not exists.');
+            exit(1);
+        }
+        $this->info("Build Image: $image Tag: $versionString");
+
+        $this->runDockerCmd(['build', '.', '-t', $image . ':' . $versionString], $workDir);
+
+        $this->runDockerCmd(['tag', $image . ':' . $versionString, $image . ':latest'], $workDir);
+
+        $this->info("Docker image built $image:$versionString successfully.");
+
+        $this->replaceConfigKey('images.php-fpm.base-image', $image . ':' . $versionString, true, config_path('dockerized.php') );
+    }
+
+    private function buildPhpCliOctaneImage($image, $versionString): void
+    {
+        $workDir = __DIR__ . '/base-images/uxtch-php-cli-octane/';
+        if (!is_dir($workDir)) {
+            $this->error('The directory docker-images/base-images/uxtch-php-cli-octane does not exists.');
+            exit(1);
+        }
+        $this->info("Build Image: $image Tag: $versionString");
+
+        $this->runDockerCmd(['build', '.', '-t', $image . ':' . $versionString], $workDir);
+
+        $this->runDockerCmd(['tag', $image . ':' . $versionString, $image . ':latest'], $workDir);
+
+        $this->info("Docker image built $image:$versionString successfully.");
+
+        $this->replaceConfigKey('images.php-octane.base-image', $image . ':' . $versionString, true, config_path('dockerized.php') );
+    }
+
+    private function buildPhpCliImage($image, $versionString): void
+    {
+        $workDir = __DIR__ . '/base-images/uxtch-php-cli/';
+        if (!is_dir($workDir)) {
+            $this->error('The directory docker-images/base-images/uxtch-php-cli does not exists.');
+            exit(1);
+        }
+        $this->info("Build Image: $image Tag: $versionString");
+
+        $this->runDockerCmd(['build', '.', '-t', $image . ':' . $versionString], $workDir);
+
+        $this->runDockerCmd(['tag', $image . ':' . $versionString, $image . ':latest'], $workDir);
+
+        $this->info("Docker image built $image:$versionString successfully.");
+
+        $this->replaceConfigKey('dockerized.images.php-cli.base-image', $image . ':' . $versionString, true, config_path('dockerized.php') );
     }
 }
